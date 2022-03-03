809870f16542cefea705b8129686972cf27736d4
Ives M. Watanabe <ivesmw@gmail.com>
2022-03-03 11:35:09 -0300

    refac: setup for integration tests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,11 +1,6 @@
-import express from "express";
-import { router } from "./routes";
-
-const app = express();
+import { app } from "./app";
 
 const PORT = process.env.PORT || 3333;
-
-app.use(router);
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
